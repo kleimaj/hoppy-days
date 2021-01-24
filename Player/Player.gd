c7a72043fa5cfb2bba1270ae154d5be65dea448b
kleimaj <jacobakleiman@gmail.com>
2021-01-23 16:11:20 -0800

    encapsulates player animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,8 @@ const GRAVITY = 300
 const UP = Vector2(0,-1)
 const JUMP_SPEED = 5000
 
+signal animate
+
 func _physics_process(delta):
 	apply_gravity()
 	jump()
@@ -34,13 +36,14 @@ func move():
 		motion.x = 0
 
 func animate():
-	if motion.y < 0:
-		$AnimatedSprite.play("jump")
-	elif motion.x > 0:
-		$AnimatedSprite.play("walk")
-		$AnimatedSprite.set_flip_h(false)		
-	elif motion.x < 0:
-		$AnimatedSprite.play("walk")
-		$AnimatedSprite.set_flip_h(true)
-	else:
-		$AnimatedSprite.play("idle")
+	emit_signal("animate", motion)
+#	if motion.y < 0:
+#		$PlayerAnimation.play("jump")
+#	elif motion.x > 0:
+#		$PlayerAnimation.play("walk")
+#		$PlayerAnimation.set_flip_h(false)		
+#	elif motion.x < 0:
+#		$PlayerAnimation.play("walk")
+#		$PlayerAnimation.set_flip_h(true)
+#	else:
+#		$PlayerAnimation.play("idle")
