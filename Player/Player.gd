8e714b9f06decff797c3aa04be9272045713e6f5
kleimaj <jacobakleiman@gmail.com>
2021-01-25 10:32:23 -0800

    fixes is_on_ceiling bug

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,9 +3,9 @@ extends KinematicBody2D
 var motion = Vector2(0,0)
 
 const SPEED = 1500
-const GRAVITY = 300
+const GRAVITY = 150
 const UP = Vector2(0,-1)
-const JUMP_SPEED = 5000
+const JUMP_SPEED = 3500
 
 signal animate
 
@@ -18,10 +18,12 @@ func _physics_process(delta):
 
 
 func apply_gravity():
-	if not is_on_floor():
-		motion.y += GRAVITY
-	else:
+	if is_on_floor():
 		motion.y = 0
+	elif is_on_ceiling():
+		motion.y = 1
+	else:
+		motion.y += GRAVITY
 	
 func jump():
 	if Input.is_action_pressed("jump") and is_on_floor():
